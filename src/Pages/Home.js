ff202b914918758863a909c06fd5b3d441a3d7db
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-07-03 14:59:18 +0600

    Starting Tailwind Landing Purple

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../partials/Footer';
+import Header from '../partials/Header';
+import Packages from '../partials/Packages';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Packages />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
