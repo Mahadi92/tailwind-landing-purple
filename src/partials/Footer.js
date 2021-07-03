c60936a17f06162a8d85ec73aaef19f9bdc5bb4c
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-07-03 18:23:45 +0600

    Header & footer responsive done

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => {
     return (
         <footer className="w-full h-96 flex items-center justify-center">
             <div className="text-center pt-14 text-white">
-                <h3 className="w-72 pb-3 my-5 m-auto border-b-2 border-gray-300 border-opacity-40 font-bold text-2xl tracking-wide">Start a project with me</h3>
+                <h3 className="md:w-72 w-60 pb-3 my-5 m-auto border-b-2 border-gray-300 border-opacity-40 font-bold md:text-2xl text-xl tracking-wide">Start a project with me</h3>
 
-                <p className="w-3/5 m-auto">Let's discuss our ideas for presenting your business to the world in an attractive, effcient and effective way.</p>
+                <p className="md:w-3/5 w-4/5 m-auto">Let's discuss our ideas for presenting your business to the world in an attractive, effcient and effective way.</p>
 
-                <button className="py-2 px-10 text-sm rounded-full bg-white text-purple-800 uppercase font-semibold shadow-xl mt-10">Hire Me</button>
+                <button className="py-2 md:px-10 px-20 text-sm rounded-full bg-white text-purple-800 uppercase font-semibold shadow-xl mt-10">Hire Me</button>
 
 
             </div>
--- src/partials/Header.js
+++ src/partials/Header.js
@@ -8,23 +8,23 @@ const Header = () => {
             <Navbar />
             <hr />
 
-            <section className="text-white flex items-center justify-evenly h-full">
-                <div className="w-2/5">
-                    <p className="uppercase">Business workflow</p>
-                    <h1 className="font-black text-5xl">Get the most</h1>
-                    <h1 className="font-black text-5xl">efficient UI design for</h1>
-                    <h1 className="font-black text-5xl">your business now!</h1>
+            <section className="text-white flex md:flex-row flex-col items-center md:justify-evenly h-full">
+                <div className="md:w-2/5 md:text-left text-center">
+                    <p className="uppercase mt-5 md:py-6 py-4">Business workflow</p>
+                    <h1 className="font-black md:text-5xl text-3xl leading-tight">Get the most</h1>
+                    <h1 className="font-black md:text-5xl text-3xl leading-tight">efficient UI design for</h1>
+                    <h1 className="font-black md:text-5xl text-3xl leading-tight">your business now!</h1>
 
-                    <p className="py-8 w-4/5">Hire me to design a a clean and modern UI for you product's website.</p>
+                    <p className="md:py-8 py-4 w-4/5 md:ml-0 m-auto">Hire me to design a a clean and modern UI for you product's website.</p>
 
-                    <div className="pt-10">
-                        <button className="py-2 px-8 text-sm border-2 border-white rounded-full bg-white text-purple-800 uppercase font-semibold mr-4">Hire Me</button>
-                        <button className="py-2 px-8 text-sm border-2 border-white rounded-full text-white uppercase">Portfolio</button>
+                    <div className="md:pt-10 pt-5 pb-14">
+                        <button className="py-2 md:px-8 px-14 text-sm border-2 border-white rounded-full bg-white text-purple-800 uppercase font-semibold md:mr-4 mr-2">Hire Me</button>
+                        <button className="py-2 md:px-8 px-10 text-sm border-2 border-white rounded-full text-white uppercase">Portfolio</button>
 
                     </div>
                 </div>
-                <div className="w-2/5">
-                    <img src={headerImage} className="w-4/5" alt="" />
+                <div className="md:w-2/5">
+                    <img src={headerImage} className="w-4/5 m-auto" alt="" />
                 </div>
             </section>
 
