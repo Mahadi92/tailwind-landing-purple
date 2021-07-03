8b00ff8ec6a19c6beafa71b081aecdeb0c781293
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-07-03 16:08:30 +0600

    header part done

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Navbar from './Navbar';
+import headerImage from '../images/image_01.png';
 
 const Header = () => {
     return (
-        <header>
+        <header className="bg-purple-400 h-screen">
             <Navbar />
             <hr />
 
+            <section className="text-white flex items-center justify-evenly h-full">
+                <div className="w-2/5">
+                    <p className="uppercase">Business workflow</p>
+                    <h1 className="font-bolder font-black text-5xl">Get the most</h1>
+                    <h1 className="font-bolder font-black text-5xl">efficient UI design for</h1>
+                    <h1 className="font-bolder font-black text-5xl">your business now!</h1>
+
+                    <p className="py-8 w-4/5">Hire me to design a a clean and modern UI for you product's website.</p>
+
+                    <div className="pt-10">
+                        <button className="py-2 px-8 text-sm border-2 border-white rounded-full bg-white text-purple-800 uppercase font-semibold mr-4">Hire Me</button>
+                        <button className="py-2 px-8 text-sm border-2 border-white rounded-full text-white uppercase">Portfolio</button>
+
+                    </div>
+                </div>
+                <div className="w-2/5">
+                    <img src={headerImage} className="w-4/5" alt="" />
+                </div>
+            </section>
 
         </header>
     );
--- src/partials/Navbar.js
+++ src/partials/Navbar.js
@@ -4,16 +4,16 @@ import brandLogo from '../images/logo_white.png';
 const Navbar = () => {
     return (
         <div className="container px-10 m-auto">
-            <nav className="flex items-center justify-between px-3 h-14 bg-purple-400">
+            <nav className="flex items-center justify-between px-3 h-14">
                 <div>
-                    <img src={brandLogo} className="object-cover md:h-7 sm:h-6" alt="" />
+                    <img src={brandLogo} className="object-cover md:h-7 h-6" alt="" />
                 </div>
                 <ul className="flex space-x-5">
                     <li className="font-bold text-white">Home</li>
                     <li className="text-gray-100">Portfolio</li>
                 </ul>
                 <div>
-                    <button className="py-2 px-5 text-sm rounded-full bg-white text-purple-800 uppercase font-semibold">Hire Me</button>
+                    <button className="py-2 px-6 text-sm rounded-full bg-white text-purple-800 uppercase font-semibold">Hire Me</button>
                 </div>
             </nav>
 
